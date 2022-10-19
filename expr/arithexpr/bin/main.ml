056fed5f6c13e67f1cd23ea872b16fa0d9961ffa
Massimo Bartoletti <bart@unica.it>
2022-10-19 11:47:33 +0200

    arithexpr

--- expr/arithexpr/bin/main.ml
+++ expr/arithexpr/bin/main.ml
@@ -17,10 +17,7 @@ let read_line () =
 
 (* print a result *)
 
-let print_result = function
-    Bool b -> print_string (if b then "true" else "false"); print_newline()
-  | Nat n -> print_string (string_of_int n); print_newline()
-;;
+let print_exprval e = print_string (string_of_exprval e); print_newline();;
 
 (* print a trace *)
 
@@ -33,16 +30,16 @@ let rec print_trace = function
 match Array.length(Sys.argv) with
   1 ->
   let s_exp = (match read_line() with Some s -> s | _ -> "")
-  in parse s_exp |> eval |> print_result
+  in parse s_exp |> eval |> print_exprval
 | 2 when Sys.argv.(1) = "test" ->
   let s_exp = read_file "test2"
-  in parse s_exp |> eval |> print_result
+  in parse s_exp |> eval |> print_exprval
 | 2 when Sys.argv.(1) = "trace" ->
   let s_exp = (match read_line() with Some s -> s | _ -> "")
   in parse s_exp |> trace |> print_trace
 | 2 ->
   let s_exp = read_file Sys.argv.(1)    
-  in parse s_exp |> eval |> print_result
+  in parse s_exp |> eval |> print_exprval
 | 3 when Sys.argv.(1) = "trace" ->
   let s_exp = read_file Sys.argv.(2)
   in parse s_exp |> trace |> print_trace     
--- expr/arithexpr/test/arithexpr.ml
+++ expr/arithexpr/test/arithexpr.ml
@@ -1,46 +1,31 @@
 open ArithexprLib.Main
+ 
+let tests = [
+  ("false",Bool false);
+  ("if true then false else true",Bool false);
+  ("if true then (if true then false else true) else (if true then true else false)",Bool false);
+  ("if (if false then false else false) then (if false then true else false) else (if true then false else true)",Bool false);
+  ("if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)",Bool false);
+  ("not true or true",Bool true);
+  ("not true and false",Bool false);
+  ("false and false or true",Bool true);
+  ("true or false and false",Bool true);
+  ("if true then true else false and false",Bool true);
+  ("if true then false else false or true",Bool false);
+  ("succ 0",Nat 1);
+  ("succ succ succ pred pred succ succ pred succ pred succ 0", Nat 3);
+  ("iszero pred succ 0", Bool true);
+  ("iszero pred succ 0 and not iszero succ pred succ 0", Bool true);
+]
 
-let s1 = "false"
-
-let s2 = "if true then false else true"
-
-let s3 = "if true then (if true then false else true) else (if true then true else false)"
-  
-let s4 = "if (if false then false else false) then (if false then true else false) else (if true then false else true)"
-
-let s5 = "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)"
-
-let s6 = "not true or true"
-
-let s7 = "not true and false"
-
-let s8 = "false and false or true"
-
-let s9 = "true or false and false"
-
-let s10 = "if true then true else false and false"
-
-let s11 = "if true then false else false or true"
-
-let s12 = "succ 0"
-
-let s13 = "iszero pred succ 0"
-
-let s14 = "iszero pred succ 0 and not iszero succ pred succ 0"
-
-let%test _ =
-  s1 |> parse |> eval = Bool false &&
-  s2 |> parse |> eval = Bool false &&
-  s3 |> parse |> eval = Bool false &&    
-  s4 |> parse |> eval = Bool false &&
-  s5 |> parse |> eval = Bool false &&
-  s6 |> parse |> eval = Bool true &&
-  s7 |> parse |> eval = Bool false &&
-  s8 |> parse |> eval = Bool true &&
-  s9 |> parse |> eval = Bool true &&
-  s10 |> parse |> eval = Bool true &&
-  s11 |> parse |> eval = Bool false &&
-  s12 |> parse |> eval = Nat 1 &&
-  s13 |> parse |> eval = Bool true &&
-  s14 |> parse |> eval = Bool true
+let%test _ = List.fold_left
+    (fun b (s,v) ->
+       print_string (s ^ " => ");
+       let b' = ((s |> parse |> eval) = v) in
+       print_string (string_of_exprval v);
+       print_string (" " ^ (if b' then "[OK]" else "[NO]"));
+       print_newline();
+       b && b')
+    true
+    tests
 
