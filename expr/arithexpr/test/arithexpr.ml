111338ec677410058d76ac951a87560255edd8de
Massimo Bartoletti <bart@unica.it>
2022-10-20 07:47:23 +0200

    arithexpr/test

--- expr/arithexpr/test/arithexpr.ml
+++ expr/arithexpr/test/arithexpr.ml
@@ -1,5 +1,17 @@
 open ArithexprLib.Main
- 
+
+(* wrapping results for testing *)
+
+type wexprval = Ok of exprval | Error
+
+let string_of_wval = function 
+    Ok v -> string_of_val v
+  | _ -> "Error"
+
+let weval e = try Ok (eval e) 
+  with _ -> Error
+;;
+  
 let tests = [
   ("false",Bool false);
   ("if true then false else true",Bool false);
@@ -18,14 +30,24 @@ let tests = [
   ("iszero pred succ 0 and not iszero succ pred succ 0", Bool true);
 ]
 
+let oktests = List.map (fun (x,y) -> (x,Ok y)) tests;;
+
+let errtests = [
+  ("iszero true", Error);
+  ("succ iszero 0", Error);
+  ("not 0", Error);
+  ("pred 0", Error);
+  ("pred pred succ 0", Error)
+]
+
 let%test _ = List.fold_left
     (fun b (s,v) ->
        print_string (s ^ " => ");
-       let b' = ((s |> parse |> eval) = v) in
-       print_string (string_of_val v);
+       let b' = ((s |> parse |> weval) = v) in
+       print_string (string_of_wval v);
        print_string (" " ^ (if b' then "[OK]" else "[NO]"));
        print_newline();
        b && b')
     true
-    tests
+    (oktests @ errtests)
 
